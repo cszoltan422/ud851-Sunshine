44e8cf19f1b58e8a0d84ee5f887a2f041093698f
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-10 15:38:16 +0100

    Instead of setting the text to , set the adapter to null before refreshing

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -192,8 +192,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            // TODO (46) Instead of setting the text to "", set the adapter to null before refreshing
-            mWeatherTextView.setText("");
+            // Instead of setting the text to "", set the adapter to null before refreshing - DONE
+            recyclerView.setAdapter(null);
             loadWeatherData();
             return true;
         }
