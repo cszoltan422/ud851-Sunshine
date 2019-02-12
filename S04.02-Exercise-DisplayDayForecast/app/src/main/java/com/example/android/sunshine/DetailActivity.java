6010373754200d376bb6775b2fec7cfc319a3639
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-12 21:40:56 +0100

    Display the weather forecast that was passed from MainActivity

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private TextView weatherDetailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        weatherDetailTextView = findViewById(R.id.weatherDetailTextView);
+
+        // Display the weather forecast that was passed from MainActivity - DONE
+        Intent intent = getIntent();
+        if (intent.hasExtra(MainActivity.DETAIL_ACTIVITY_WEATHER_FOR_DAY)) {
+            weatherDetailTextView.setText(intent.getStringExtra(MainActivity.DETAIL_ACTIVITY_WEATHER_FOR_DAY));
+        }
     }
 }
